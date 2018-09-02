c01649dd7520e6336d3716c30da653f4843cf321
Adam Clayman <adam@clayman.org>
2018-09-02 18:16:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     data_raw = filename.split(".mp3")
     data = data_raw[0].split(" - ")
     song = Song.new(data[1])
-    artist = artist.find_or_create_by_name(data[0])
+    artist = Artist.find_or_create_by_name(data[0])
     song.artist = artist
     song.artist.songs << song
     song.genre = data[2]
