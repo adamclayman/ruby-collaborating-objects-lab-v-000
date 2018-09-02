30aa6fc58e934329e9f0be88e032c76a0a779eb0
Adam Clayman <adam@clayman.org>
2018-09-02 18:09:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,10 @@ class Artist
     @@all << self
   end
 
+  def add_song(song)
+    self.songs << song
+  end
+
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) != nil
       return self.find_by_name(name)
