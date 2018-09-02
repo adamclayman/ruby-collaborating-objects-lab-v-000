f30e919ac023024ca36242847ece3b147bc63197
Adam Clayman <adam@clayman.org>
2018-09-02 18:15:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @name = name
   end
 
-  def new_by_filename(filename)
+  def self.new_by_filename(filename)
     data_raw = filename.split(".mp3")
     data = data_raw[0].split(" - ")
     song = Song.new(data[1])
