874dbe82e3e475afbee6d8ca10c7435ac749d2ca
Adam Clayman <adam@clayman.org>
2018-09-02 18:08:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def new_by_filename(filename)
     data = filename.split(" - ")
     song = Song.new(data[1])
-    artist = Artist.new(data[0])
+    artist = artist.find_or_create_by_name(data[0])
     song.artist = artist
     song.artist.songs << song
     song.genre = data[2]
