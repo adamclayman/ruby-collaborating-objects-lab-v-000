95fed234ad099917e8c0268b83d793d682e9f307
Adam Clayman <adam@clayman.org>
2018-09-02 18:13:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,8 @@ class Song
   end
 
   def new_by_filename(filename)
-    data = filename.split(" - ")
-    data = data.slice(0,2)
+    data_raw = filename.split(".mp3")
+    data = data_raw[0].split(" - ")
     song = Song.new(data[1])
     artist = artist.find_or_create_by_name(data[0])
     song.artist = artist
